f24cc19480c86d4c7cb3438d26d164de6f39eb74
DolimovS <samandardolimov71@gmail.com>
2025-08-06 23:24:18 +0500

    movies add form

--- src/components/App/App.vue
+++ src/components/App/App.vue
@@ -1,14 +1,13 @@
 <template>
   <div class="app font-monospace">
     <div class="content">
-      <AppInfo :allMoviesCount="movies.length"
-      :favouriteMoviesCount="movies.filter(c=>c.favourite).length"/>
+      <AppInfo :allMoviesCount="movies.length" :favouriteMoviesCount="movies.filter(c => c.favourite).length" />
       <div class="search-panel">
-        <SearchPanel/>
-        <AppFilter/>
+        <SearchPanel />
+        <AppFilter />
       </div>
-      <MovieList :movies="movies"/>
-      <MovieAddForm/>
+      <MovieList :movies="movies" />
+      <MovieAddForm @movieAdded="moviePush" />
     </div>
   </div>
 </template>
@@ -20,40 +19,47 @@ import AppInfo from '../app-info/Appinfo.vue';
 import MovieAddForm from '../movie-add-form/MovieAddForm.vue';
 import MovieList from '../movie-list/MovieList.vue';
 import SearchPanel from '../search-panel/SearchPanel.vue';
-  export default {
-    components: {
-      AppInfo,
-      SearchPanel,
-      AppFilter,
-      MovieList,
-      MovieAddForm,
-    
-    },
-    data() {
-        return {
-            movies: [
-                {
-                    name: 'Omar',
-                    viewers: 890,
-                    favourite:false,
-                    like:true,
-                },
-                {
-                    name: 'Emoire of osman',
-                    viewers: 356,
-                    favourite:false,
-                    like:false,
-                },
-                {
-                    name: 'Ertugrul',
-                    viewers: 494,
-                    favourite:true,
-                    like:false,
-                }
-            ]
+export default {
+  components: {
+    AppInfo,
+    SearchPanel,
+    AppFilter,
+    MovieList,
+    MovieAddForm,
+
+  },
+  data() {
+    return {
+      movies: [
+        {
+          name: 'Omar',
+          viewers: 890,
+          favourite: false,
+          like: true,
+        },
+        {
+          name: 'Emoire of osman',
+          viewers: 356,
+          favourite: false,
+          like: false,
+        },
+        {
+          name: 'Ertugrul',
+          viewers: 494,
+          favourite: true,
+          like: false,
         }
+      ]
+    }
+  },
+  methods: {
+    moviePush(item) {
+      console.log(item);
+      this.movies.push(item);
+      console.log(this.movies);
     }
   }
+}
 </script>
 
 <style scoped>
@@ -61,18 +67,20 @@ import SearchPanel from '../search-panel/SearchPanel.vue';
   height: 100vh;
   color: black;
 }
-.content{
+
+.content {
   width: 1000px;
   min-height: 700px;
   background-color: #fff;
   margin: 0 auto;
   padding: 5rem 0;
 }
-.search-panel{
+
+.search-panel {
   margin-top: 2rem;
   padding: 1.5rem;
   background-color: #fcfaf5;
   border-radius: 4px;
-  box-shadow: 15px 15px 15px rgba(0,0,0,0.15);
+  box-shadow: 15px 15px 15px rgba(0, 0, 0, 0.15);
 }
 </style>
--- src/components/movie-add-form/MovieAddForm.vue
+++ src/components/movie-add-form/MovieAddForm.vue
@@ -1,18 +1,45 @@
 <template>
     <div class="movie-add-form">
         <h3>Yangi kino qo'shish</h3>
-        <form class="add-form d-flex">
-            <input type="text" class="form-control new-movie-label" placeholder="Qanday kino">
-             <input type="text" class="form-control new-movie-label" placeholder="Necha marta ko'rilgan?">
+        <form  @submit.prevent="addMovie" class="add-form d-flex" >
+            <input 
+            v-model="addName" 
+            type="text" 
+            class="form-control new-movie-label" 
+            placeholder="Qanday kino">
+             <input 
+             v-model="addViewers" 
+             type="number" 
+             class="form-control new-movie-label" 
+             placeholder="Necha marta ko'rilgan?">
 
-             <button class="btn btn-outline-dark" type="submit">Qo'shish</button>
+             <button  class="btn btn-outline-dark" type="submit">Qo'shish</button>
         </form>
     </div>
 </template>
 
 <script>
     export default {
-        
+        data(){
+           return{
+             addName:'',
+             addViewers:'',
+           }
+        },
+        methods:{
+            addMovie(){
+               const newMovie={
+                name:this.addName,
+                viewers:this.addViewers,
+                favourite:false,
+                like:false,
+            } 
+            this.$emit('movieAdded',newMovie)
+            console.log(newMovie);
+            this.addName='';
+            this.addViewers='';
+            }
+        }
     }
 </script>
 
